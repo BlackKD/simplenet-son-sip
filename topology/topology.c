a12859c31844ea34e48819d2662f7596e545a99a
BlackKD <836124576@qq.com>
2016-05-03 16:37:41 +0800

    e

--- topology/topology.c
+++ topology/topology.c
@@ -17,10 +17,10 @@ extern unsigned long get_local_ip();
 int myNodeId = 0;
 
 FILE *open_dat() {
-	FILE *fp=fopen("topology/topology.dat","r");
+	FILE *fp=fopen("../topology/topology.dat","r");
     if(fp==NULL)//如果失败了
     {
-        perror("topology/topology.dat");
+        perror("../topology/topology.dat");
         //printf("错误！");
         exit(1);//中止程序
     }
